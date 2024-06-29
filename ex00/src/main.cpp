c19bf328f2951c2179a842e3f53b1979bb8828a0
Toshiki Sakai <tosshi18@gmail.com>
2024-06-30 05:55:06 +0900

    change main.cpp to apply submitt  format

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -2,152 +2,18 @@
 #include <iostream>
 #include <cstdlib>
 
-int main()
+int main(int argc, const char *argv[])
 {
-    {
-        std::string literal = "0";
-        try {
-            std::cout << "========= subject 0=========" << std::endl;
-            std::cout << "char: Non displayable" << std::endl;
-            std::cout << "int: 0" << std::endl;
-            std::cout << "float: 0.0f" << std::endl;
-            std::cout << "double: 0.0" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "";
-        try {
-            std::cout << "========= subject '' =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "\n";
-        try {
-            std::cout << "========= subject \\n =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "nan";
-        try {
-            std::cout << "========= subject nan =========" << std::endl;
-            std::cout << "char: impossible" << std::endl;
-            std::cout << "int: impossible" << std::endl;
-            std::cout << "float: nanf" << std::endl;
-            std::cout << "double: nan" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-            std::cout << "======== chatgpt ==========" << std::endl;
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "42.0f";
-        try {
-            std::cout << "========= subject 42.0f =========" << std::endl;
-            std::cout << "char: '*'" << std::endl;
-            std::cout << "int: 42" << std::endl;
-            std::cout << "float: 42.0f" << std::endl;
-            std::cout << "double: 42.0" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "nanf";
-        try {
-            std::cout << "========= other test nanf =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "2147483647";
-        try {
-            std::cout << "========= other test INT_MAX =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "2147483648";
-        try {
-            std::cout << "========= other test INT_MAX+1 =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "-2147483648";
-        try {
-            std::cout << "========= other test INT_MIN =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "-2147483649";
-        try {
-            std::cout << "========= other test INT_MIN-1 =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "abc";
-        try {
-            std::cout << "========= other test: abc =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    {
-        std::string literal = "abc.0f";
-        try {
-            std::cout << "========= other test: abc =========" << std::endl;
-            std::cout << "========= my ==============" << std::endl;
-            ScalarConverter::convert(literal);
-        } catch (std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-    }   
-    return EXIT_SUCCESS;
+    if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <put string for exchanging to data type(char/int/float/double)>"
+              << std::endl;
+    return EXIT_FAILURE;
+    }
+    try {
+        ScalarConverter::convert(argv[1]);
+        return EXIT_SUCCESS;
+    } catch (std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 }
